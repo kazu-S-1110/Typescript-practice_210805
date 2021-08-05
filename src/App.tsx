f5e0ea6e7c47ce0eb11f71429ac9ed1bdeb299a8
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-06 07:59:55 +0900

    axiosをimport。コードを一旦整形。

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Practice1 } from "./practices/Practice1";
-import { Practice2 } from "./practices/Practice2";
-import { Practice3 } from "./practices/Practice3";
-import { Practice4 } from "./practices/Practice4";
 import "./styles.css";
+import axios from "axios";
 
 export default function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res.data);
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
-      <Practice4 />
+      <button onClick={onClickFetchData}>Fetch</button>
     </div>
   );
 }
