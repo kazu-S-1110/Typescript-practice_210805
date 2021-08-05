b52522254f75fad62af55ae51e36529b6bad1a3c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-06 08:26:30 +0900

    さらに詳しくhooksの型指定を学習。これが知りたかった！！

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,29 @@ import axios from "axios";
 import { useState } from "react";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]); //stateに対しての型指定はhooksの後ろに書く。
+  const [todos, setTodos] = useState<Array<TodoType>>([]); //stateに対しての型指定はhooksの後ろに書く。
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res.data);
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        console.log(res.data);
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Fetch</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userid} />
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
