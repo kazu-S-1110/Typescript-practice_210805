258e8a808ec925fc64ca7d50cc535b12cc52d409
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-06 12:34:52 +0900

    propsの型定義を学習。これはよく使うからちゃんと覚えよう。

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,3 +1,18 @@
-export const Todo = ({ title, userid }) => {
-  return <p>{`${title}(ユーザid:${userid})`}</p>;
+import { VFC } from "react";
+
+type PropType = {
+  userId: number;
+  title: string;
+  completed?: boolean; //?をつけることでオプショナルとなる
+};
+
+export const Todo: VFC<PropType> = ({ title, userId, completed = false }) => {
+  return (
+    <>
+      <p>
+        {`${title}(ユーザid:${userId})`}
+        <input type="checkbox" name="complete" defaultChecked={completed} />
+      </p>
+    </>
+  );
 };
