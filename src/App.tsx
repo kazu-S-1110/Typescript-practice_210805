73bef99d7164158e7be50a02e09a107e39db20ef
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-06 12:46:08 +0900

    型情報を効率的にする方法を学習。（Pick,Omit）

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import "./styles.css";
 import axios from "axios";
 import { useState } from "react";
 import { Todo } from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]); //stateに対しての型指定はhooksの後ろに書く。
@@ -26,6 +20,7 @@ export default function App() {
       <button onClick={onClickFetchData}>Fetch</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,12 +1,14 @@
 import { VFC } from "react";
+import { TodoType } from "./types/todo";
 
-type PropType = {
-  userId: number;
-  title: string;
-  completed?: boolean; //?をつけることでオプショナルとなる
-};
-
-export const Todo: VFC<PropType> = ({ title, userId, completed = false }) => {
+//Pickを使って既存の型を抜き出す手法あり。
+// export const Todo: VFC<Pick<TodoType, "title" | "userId" | "completed">> = ({
+//Omit（除外する）を使う方法もあり。
+export const Todo: VFC<Omit<TodoType, "id">> = ({
+  title,
+  userId,
+  completed = false
+}) => {
   return (
     <>
       <p>
