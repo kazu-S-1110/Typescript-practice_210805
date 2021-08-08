3a5fbb40c5ff648efb2f73577f942d5c478a0e84
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-08 21:13:57 +0900

    カスタムフックを使わないベーシックなAPI取得をしてみた。

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,52 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
-import { UserProfile } from "./UserProfile";
-import { User } from "./User";
+// import { UserProfile } from "./UserProfile";
+import { UserCard } from "./components/UserCard";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfiles";
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<TodoType>>([]); //stateに対しての型指定はhooksの後ろに書く。
-  const user: User = {
-    name: "puppy"
+  // const [todos, setTodos] = useState<Array<TodoType>>([]); //stateに対しての型指定はhooksの後ろに書く。
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const User: UserProfile = {
+    id: 1,
+    name: "puppy",
+    email: "test@test.com",
+    address: "Address"
     // hobbies: ["game", "programming"]
   };
   const onClickFetchData = () => {
     axios
-      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
-        console.log(res.data);
-        setTodos(res.data);
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
       });
   };
 
   return (
     <div className="App">
-      <UserProfile user={user} />
+      {userProfiles.map((user) => (
+        <UserCard user={user} />
+      ))}
+
+      {/* <UserProfile user={user} /> */}
       <button onClick={onClickFetchData}>Fetch</button>
-      <Text color="red" fontSize="30px" />
-      {todos.map((todo) => (
+      {/* <Text color="red" fontSize="30px" /> */}
+      {/* {todos.map((todo) => (
         <Todo
           key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
