aaf52314ca411bd3d1920d1a3a1479696f779274
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-08 23:14:31 +0900

    カスタムフックの作成を学習。ロジック部分をただ切り出すだけなのかな、、？

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ import { UserProfile } from "./types/userProfiles";
 
 export default function App() {
   // const [todos, setTodos] = useState<Array<TodoType>>([]); //stateに対しての型指定はhooksの後ろに書く。
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+
   // const User: UserProfile = {
   //   id: 1,
   //   name: "puppy",
@@ -21,23 +19,7 @@ export default function App() {
   //   address: "Address"
   //   // hobbies: ["game", "programming"]
   // };
-  const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => setError(true))
-      .finally(() => setLoading(false));
-  };
+  const onClickFetchData = () => {};
 
   return (
     <div className="App">
